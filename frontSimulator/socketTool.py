71a4e22a00b909de91d47541058002fa25f41876
zhhkof <416799337@qq.com>
2016-12-26 15:17:30 +0800

    tcp func finished.

--- frontSimulator/socketTool.py
+++ frontSimulator/socketTool.py
@@ -23,12 +23,8 @@ def multicast_send(SENDERIP, SENDERPORT, MYGROUP, MYPORT, SENDDATA='testdata'):
     status = s.setsockopt(IPPROTO_IP,
                           IP_ADD_MEMBERSHIP,
                           inet_aton(MYGROUP) + inet_aton(SENDERIP))  # 加入组播组
-    # for i in range(times):
-    # data = (time.strftime('%Y-%m-%d %H:%M:%S') + ': multicast test data.').encode('utf-8')  # socket发送需转成byte字节。
     s.sendto(SENDDATA.encode('utf-8'), (MYGROUP, MYPORT))
-    # print(str(i) + ":" + SENDDATA)
     print("send data ok !")
-    # time.sleep(1)
 
 
 # def multicast_send2(**kwargs):
